9d41763588e84fe4f2e21bdbc2acc8b3ad18eead
jarrodek <jarrodek@gmail.com>
2016-09-29 18:36:10 +0100

    Finished CI

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.use(bodyParser.raw({type: 'application/*'}));
 
 class ArcCiServer {
 
+  get elementsParents() {
+    return ['chrome-elements', 'logic-elements', 'raml-elements', 'transport-elements',
+      'ui-elements'];
+  }
+
+  get nonElements() {
+    return ['arc-datastore', 'arc-tools', 'polymd', 'cookie-parser',
+    'har', 'arc-element-catalog', 'ci-server'];
+  }
+
+  get ignored() {
+    return this.nonElements.concat(this.elementsParents);
+  }
+
   constructor() {
     this.port = 5243;
     app.disable('x-powered-by');
@@ -23,7 +37,7 @@ class ArcCiServer {
 
   setHandlers() {
     this.setStageUpdate();
-    this.setTestServiice();
+    // this.setTestServiice();
   }
 
   createServer() {
@@ -66,7 +80,7 @@ class ArcCiServer {
       res.set('Connection', 'close');
       let body = JSON.parse(decoder.write(req.body));
       if (event === 'pull_request') {
-        this.handlePullRequest(body);
+        // this.handlePullRequest(body);
         return res.sendStatus(204);
       }
       if (event === 'push') {
@@ -76,10 +90,10 @@ class ArcCiServer {
     });
   }
 
-  handlePullRequest(body) {
-    // empty for now
-    console.log('handlePullRequest');
-  }
+  // handlePullRequest(body) {
+  //   // empty for now
+  //   console.log('handlePullRequest');
+  // }
 
   handlePush(body) {
     var branch = body.ref;
@@ -87,31 +101,48 @@ class ArcCiServer {
       console.log('No ref...');
       return;
     }
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    var message = body.head_commit.message;
+    var repoName = body.repository.name;
+    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+    if (this.ignored.indexOf(repoName) !== -1) {
+      return; // Don't process this repos.
+    }
     if (branch === 'refs/heads/stage') {
-      if (body.head_commit.message === 'Initial commit') {
+      if (message === 'Initial commit') {
         // Just quietly exit
         return;
       }
 
       // Check if this isn't our own change to stage
-      if (body.head_commit.message.indexOf('[CI]') === 0) {
+      if (message.indexOf('[CI]') === 0) {
         console.log('Dropping task. It\'s an CI commit.');
         console.log();
         return;
       }
-
-      let repoName = body.repository.name;
       this.handleStageBuild(repoName);
-    } else if ('refs/heads/master') {
-      if (body.head_commit.message === 'Initial commit') {
+    } else if (branch === 'refs/heads/master') {
+      if (message === 'Initial commit') {
         // Just quietly exit
         return;
       }
-      if (body.head_commit.message === '[CI] Automated merge stage->master') {
-        let repoName = body.repository.name;
+      if (message === '[CI] Automated merge stage->master') {
         this.handleRelease(repoName);
       } else {
         console.log('Unsuppoeted master branch');
+      }
+    } else if (branch.indexOf('refs/tags/') === 0) {
+      // if (message.indexOf('[CI]') !== -1) {
+      //   console.log('Dropping task. It\'s an CI commit.');
+      //   console.log();
+      //   return;
+      // }
+      if (this.elementsParents.indexOf(repoName) !== -1) {
+        // this is a parent element and it can't be processed.
+        return;
+      }
+      if (message === '[CI] Automated merge stage->master') {
+        this._processNewTag(repoName);
       }
     } else {
       console.log('Dropping branch ' + branch);
@@ -131,6 +162,16 @@ class ArcCiServer {
     }
     this._runScript('./tag-build', [name], 'release');
   }
+
+  _processNewTag(name) {
+    console.log('Updating elements structure: ' + name);
+    if (!process.env.GITHUB_TOKEN) {
+      console.error('process.env.GITHUB_TOKEN IS UNAVAILABLE');
+      return;
+    }
+    this._runScript('./update-structure', [name], 'structure');
+  }
+
   /**
    * Run a bash file
    *
@@ -138,31 +179,41 @@ class ArcCiServer {
    * @param {Array<String>} Array of parameters
    */
   _runScript(file, params, name) {
-    try {
-      const spawn = require('child_process').spawn;
-      const build = spawn(file, params);
+    return new Promise((resolve, reject) => {
+      try {
+        const spawn = require('child_process').spawn;
+        const build = spawn(file, params);
 
-      build.stdout.on('data', (data) => {
-        if (!data) {
-          return;
-        }
-        console.log(`[${name}]: ${data}`);
-      });
+        build.stdout.on('data', (data) => {
+          if (!data) {
+            return;
+          }
+          console.log(`[${name}]: ${data}`);
+        });
 
-      build.stderr.on('data', (data) => {
-        if (!data) {
-          return;
-        }
-        console.error(`[${name}]: ${data}`);
-      });
+        build.stderr.on('data', (data) => {
+          if (!data) {
+            return;
+          }
+          console.error(`[${name}]: ${data}`);
+        });
 
-      build.on('close', (code) => {
-        console.log(`[${name}] finished with code ${code}`);
-      });
-    } catch (e) {
-      console.error(`[${name}] fatal error: ${e.message}`);
-    }
+        build.on('close', (code) => {
+          console.log(`[${name}] finished with code ${code}`);
+          if (code === 0 || code === '0') {
+            resolve();
+          } else {
+            reject(new Error(`task [${name}] finished with code ${code}`));
+          }
+        });
+      } catch (e) {
+        var msg = `[${name}] fatal error: ${e.message}`;
+        console.error(msg);
+        reject(msg);
+      }
+    });
   }
+
 }
 
 new ArcCiServer();
