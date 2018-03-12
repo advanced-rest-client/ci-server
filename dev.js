2be6b7e3cb2d1547f573626dc65a5d9b71ef6c0d
CI <arc@mulesoft.com>
2018-03-11 17:29:01 -0700

    Update: Added keys normalization.

--- dev.js
+++ dev.js
@@ -2,3 +2,16 @@ const {CatalogDataGenerator} = require('./lib/data-generator');
 const builder = new CatalogDataGenerator('xml-viewer', '2.0.0');
 builder.build()
 .catch((e) => console.error(e));
+// const slug = require('slug');
+// const decamelize = require('decamelize');
+
+// slug.defaults.mode ='pretty';
+// slug.defaults.modes.pretty = {
+//     replacement: '-',
+//     symbols: true,
+//     remove: /[.]/g,
+//     lower: true,
+//     charmap: slug.charmap,
+//     multicharmap: slug.multicharmap
+// };
+// console.log(slug(decamelize('UiElements', '-')));
